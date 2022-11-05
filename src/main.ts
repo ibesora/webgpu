fe4546e6c80829c4954d0ebe928fa7fe22740fc3
Isaac Besora <isaac.besora@gmail.com>
2022-11-05 20:54:01 +0100

    Cube with light

--- src/main.ts
+++ src/main.ts
@@ -1,175 +1,34 @@
-import {
-  InitGPU,
-  CreateGPUBuffer,
-  CreateTransforms,
-  CreateViewProjection,
-  CreateAnimation,
-} from "./helper";
-import shader from "./shader.wgsl";
-import "./site.css";
+import { CreateShapeWithLight, LightInputs } from "./light";
 import { CubeData } from "./vertex-data";
-import { vec3, mat4 } from "gl-matrix";
 import $ from "jquery";
-const createCamera = require("3d-view-controls");
+import "./site.css";
 
-const Create3DObject = async (isAnimation = true) => {
-  const gpu = await InitGPU();
-  const device = gpu.device;
+const data = CubeData();
+let li: LightInputs = {};
+let isAnimation = true;
+CreateShapeWithLight(data.positions, data.normals, li, isAnimation);
 
-  // create vertex buffers
-  const cubeData = CubeData();
-  const numberOfVertices = cubeData.positions.length / 3;
-  const vertexBuffer = CreateGPUBuffer(device, cubeData.positions);
-  const colorBuffer = CreateGPUBuffer(device, cubeData.colors);
-
-  const pipeline = device.createRenderPipeline({
-    layout: "auto",
-    vertex: {
-      module: device.createShaderModule({
-        code: shader,
-      }),
-      entryPoint: "vs_main",
-      buffers: [
-        {
-          arrayStride: 12,
-          attributes: [
-            {
-              shaderLocation: 0,
-              format: "float32x3",
-              offset: 0,
-            },
-          ],
-        },
-        {
-          arrayStride: 12,
-          attributes: [
-            {
-              shaderLocation: 1,
-              format: "float32x3",
-              offset: 0,
-            },
-          ],
-        },
-      ],
-    },
-    fragment: {
-      module: device.createShaderModule({
-        code: shader,
-      }),
-      entryPoint: "fs_main",
-      targets: [
-        {
-          format: gpu.format as GPUTextureFormat,
-        },
-      ],
-    },
-    primitive: {
-      topology: "triangle-list",
-      cullMode: "back",
-    },
-    depthStencil: {
-      format: "depth24plus",
-      depthWriteEnabled: true,
-      depthCompare: "less",
-    },
-  });
-
-  // create uniform data
-  const modelMatrix = mat4.create();
-  const mvpMatrix = mat4.create();
-  let vMatrix = mat4.create();
-  let vpMatrix = mat4.create();
-  const vp = CreateViewProjection(gpu.canvas.width / gpu.canvas.height);
-  vpMatrix = vp.viewProjectionMatrix;
-
-  // add rotation and camera:
-  let rotation = vec3.fromValues(0, 0, 0);
-  var camera = createCamera(gpu.canvas, vp.cameraOption);
-
-  // create uniform buffer and bind group
-  const uniformBuffer = device.createBuffer({
-    size: 64,
-    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-  });
-
-  const uniformBindGroup = device.createBindGroup({
-    layout: pipeline.getBindGroupLayout(0),
-    entries: [
-      {
-        binding: 0,
-        resource: {
-          buffer: uniformBuffer,
-          offset: 0,
-          size: 64,
-        },
-      },
-    ],
-  });
-
-  let textureView = gpu.context.getCurrentTexture().createView();
-  const depthTexture = device.createTexture({
-    size: [gpu.canvas.width, gpu.canvas.height, 1],
-    format: "depth24plus",
-    usage: GPUTextureUsage.RENDER_ATTACHMENT,
-  });
-  const renderPassDescription = {
-    colorAttachments: [
-      {
-        view: textureView,
-        clearValue: { r: 0.2, g: 0.247, b: 0.314, a: 1.0 }, //background color
-        loadOp: "clear",
-        storeOp: "store",
-      },
-    ],
-    depthStencilAttachment: {
-      view: depthTexture.createView(),
-      depthLoadValue: 1.0,
-      depthClearValue: 1.0,
-      depthLoadOp: "clear",
-      depthStoreOp: "store",
-    },
-  };
-
-  function draw() {
-    if (!isAnimation) {
-      if (camera.tick()) {
-        const pMatrix = vp.projectionMatrix;
-        vMatrix = camera.matrix;
-        mat4.multiply(vpMatrix, pMatrix, vMatrix);
-      }
-    }
-
-    CreateTransforms(modelMatrix, [0, 0, 0], rotation);
-    mat4.multiply(mvpMatrix, vpMatrix, modelMatrix);
-    device.queue.writeBuffer(uniformBuffer, 0, mvpMatrix as ArrayBuffer);
-    textureView = gpu.context.getCurrentTexture().createView();
-    renderPassDescription.colorAttachments[0].view = textureView;
-    const commandEncoder = device.createCommandEncoder();
-    const renderPass = commandEncoder.beginRenderPass(
-      renderPassDescription as GPURenderPassDescriptor
-    );
-
-    renderPass.setPipeline(pipeline);
-    renderPass.setVertexBuffer(0, vertexBuffer);
-    renderPass.setVertexBuffer(1, colorBuffer);
-    renderPass.setBindGroup(0, uniformBindGroup);
-    renderPass.draw(numberOfVertices);
-    renderPass.end();
-
-    device.queue.submit([commandEncoder.finish()]);
-  }
-
-  CreateAnimation(draw, rotation, isAnimation);
-};
-
-let is_animation = true;
-Create3DObject(is_animation);
 $("#id-radio input:radio").on("click", function () {
   let val = $('input[name="options"]:checked').val();
-  is_animation = val === "animation" ? true : false;
-  Create3DObject(is_animation);
+  if (val === "animation") isAnimation = true;
+  else isAnimation = false;
+  CreateShapeWithLight(data.positions, data.normals, li, isAnimation);
+});
+
+$("#btn-redraw").on("click", function () {
+  li.color = $("#id-color").val()?.toString()?.split(",").map(Number) as any;
+  li.ambientIntensity = parseFloat($("#id-ambient").val()?.toString()!);
+  li.diffuseIntensity = parseFloat($("#id-diffuse").val()?.toString()!);
+  li.specularIntensity = parseFloat($("#id-specular").val()?.toString()!);
+  li.shininess = parseFloat($("#id-shininess").val()?.toString()!);
+  li.specularColor = $("#id-scolor")
+    .val()
+    ?.toString()
+    ?.split(",")
+    .map(Number) as any;
+  CreateShapeWithLight(data.positions, data.normals, li, isAnimation);
 });
 
 window.addEventListener("resize", function () {
-  Create3DObject(is_animation);
+  CreateShapeWithLight(data.positions, data.normals, li, isAnimation);
 });
